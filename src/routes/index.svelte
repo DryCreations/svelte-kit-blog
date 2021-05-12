2c144ad5a00e958ff77bec042b791fd613ea95fe
Hayden Mankin <DryCreations@gmail.com>
2021-05-12 17:59:58 -0400

    fix blog url

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,4 +2,4 @@
     import {base} from '$app/paths'
 </script>
 <h1>Welcome to the blog template</h1>
-<p>checkout <a href={`${base}blog`}>/blog</a> to see posts</p>
+<p>checkout <a href={`${base}/blog`}>/blog</a> to see posts</p>
