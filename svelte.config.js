7a9122be92d16cf25bf06cf4685e6fb86ac17a50
Hayden Mankin <DryCreations@gmail.com>
2021-05-12 16:47:10 -0400

    dev var

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,17 @@
 import { mdsvex } from "mdsvex";
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		target: '#svelte',
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === "development" ? '' : '/svelte-kit-blog',
-			assets: process.env.NODE_ENV === "development" ? '' : '/svelte-kit-blog'
-
-		},
-		vite: () => ({
-			// base: process.env.NODE_ENV === "development" ? '/' : '/svelte-kit-blog/'
-		})
+			base: dev ? '' : '/svelte-kit-blog',
+			assets: dev ? '' : '/svelte-kit-blog'
+		}
 	},
 	preprocess: [
 		mdsvex({ extensions: ['.svx']})
