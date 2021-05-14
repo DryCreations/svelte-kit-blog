f096d3351a269b1f35d2a0d9622387321944bd38
Hayden Mankin <DryCreations@gmail.com>
2021-05-14 03:26:43 -0400

    test header

--- src/lib/Core/Header.svelte
+++ src/lib/Core/Header.svelte
@@ -16,8 +16,8 @@
         <h1 class="text-xl">{info.title}</h1>
       </a>
       <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-        <a href="{base}/" class:border-b-2="{path == base + '/'}" class="border-indigo-600 mr-5 hover:text-gray-900">Home</a>
-        <a href="{base}/about" class:border-b-2="{path == base + '/about'}" class="border-indigo-600 mr-5 hover:text-gray-900">About</a>
-        <a href="{base}/blog" class:border-b-2="{path == base + '/blog'}" class="border-indigo-600 hover:text-gray-900">Blog</a>
+        <a href="{base}/" class:border-b-2="{path == '/'}" class="border-indigo-600 mr-5 hover:text-gray-900">Home</a>
+        <a href="{base}/about" class:border-b-2="{path == '/about'}" class="border-indigo-600 mr-5 hover:text-gray-900">About</a>
+        <a href="{base}/blog" class:border-b-2="{path == '/blog'}" class="border-indigo-600 hover:text-gray-900">Blog</a>
       </nav>
 </header>
