7b754136ad20f981cfae0e55057867520477928d
Hayden Mankin <DryCreations@gmail.com>
2021-05-16 22:54:37 -0400

    a11ty

--- src/lib/Core/Search.svelte
+++ src/lib/Core/Search.svelte
@@ -20,6 +20,7 @@ import { goto } from "$app/navigation";
 </script>
 
 <div class="flex flex-row-reverse items-center ">
+    <label>
     <input bind:this={textBox} bind:value={search} on:keydown={redirect} class="ml-5 outline-none dark:text-gray-400 text-gray-500 dark:focus:text-white focus:text-gray-900 bg-transparent w-0 focus:w-32 border-b-2 dark:focus:border-white focus:border-gray-900 dark:border-gray-400 border-gray-500" >
 
     <button on:click={focusOnSearch} class="">
@@ -28,6 +29,7 @@ import { goto } from "$app/navigation";
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
         </svg>
     </button>
+    </label>
 </div>
 
 <style>
