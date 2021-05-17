f4f13d57596bda986ccccfc04529ac2359cf1732
DryCreations <DryCreations@gmail.com>
2021-05-17 02:16:55 +0000

    Deploying to gh-pages from @ DryCreations/svelte-kit-blog@18434a2077c23329e39aedc1142a0e66609b7de7 🚀

---
+++ _app/chunks/stores-9861be58.js
@@ -0,0 +1,1 @@
+import{Q as s}from"./vendor-82f036b7.js";const e=()=>{const e=s("__svelte__");return{page:{subscribe:e.page.subscribe},navigating:{subscribe:e.navigating.subscribe},get preloading(){return console.error("stores.preloading is deprecated; use stores.navigating instead"),{subscribe:e.navigating.subscribe}},session:e.session}};export{e as g};
