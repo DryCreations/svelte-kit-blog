3e2b72b88b5a3f155bf65381bda145c986838d65
Hayden Mankin <DryCreations@gmail.com>
2021-05-17 00:53:31 -0400

    fix search button

--- src/lib/Core/Search.svelte
+++ src/lib/Core/Search.svelte
@@ -19,11 +19,11 @@ import { goto } from "$app/navigation";
     }
 </script>
 
-    <label class="flex flex-row-reverse items-center ">
+    <label class="flex flex-row-reverse items-center">
     <input bind:this={textBox} bind:value={search} on:keydown={redirect} class="ml-5 outline-none dark:text-gray-400 text-gray-500 dark:focus:text-white focus:text-gray-900 bg-transparent w-0 focus:w-32 border-b-2 dark:focus:border-white focus:border-gray-900 dark:border-gray-400 border-gray-500" >
 
     <button on:click={focusOnSearch} class="">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 dark:hover:text-white hover:text-gray-900" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 dark:hover:text-white hover:text-gray-900 " fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <title>search</title>
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
         </svg>
@@ -33,7 +33,7 @@ import { goto } from "$app/navigation";
 
 <style>
     input:focus + button {
-        @apply text-white
+        @apply dark:text-white text-gray-900
     }
 
     input {
